2118eae1c25fc53fe441f8625525d2a9e3c67468
BJChen990 <bengjing@gmail.com>
2020-05-10 00:54:14 +0800

    Refactor update and get mechanism

--- src/accessory.ts
+++ src/accessory.ts
@@ -74,82 +74,109 @@ export class AirConditionerCompanionAccessory implements AccessoryPlugin {
 
   private udpateStatus = async () => {
     const { companionStatus } = await this.device.getProperties();
-    const { thermostatService } = this;
+    const { thermostatService, fanService } = this;
     const { Characteristic } = this.homebridge.hap;
     const { TargetFanState, CurrentTemperature, TargetTemperature } = Characteristic;
-    this.logger.debug('latest status: %s', companionStatus);
-    if (companionStatus.temperature !== this.status.temperature) {
-      this.logger.debug('update target temperature -> %s', companionStatus.temperature);
-      thermostatService.updateCharacteristic(CurrentTemperature, companionStatus.temperature);
-      thermostatService.updateCharacteristic(TargetTemperature, companionStatus.temperature);
-    }
-    if (companionStatus.power !== this.status.power) {
-      this.logger.debug('update power -> %s', companionStatus.power);
-      const active = companionStatus.power === On.ON;
-      thermostatService.updateCharacteristic(
-        TargetHeatingCoolingState,
-        active ? TargetHeatingCoolingState.AUTO : TargetHeatingCoolingState.OFF,
-      );
-      this.fanService.updateCharacteristic(Active, active ? Active.ACTIVE : Active.INACTIVE);
-    }
-    if (companionStatus.rotationSpeed !== this.status.rotationSpeed) {
-      this.logger.debug('update rotation speed -> %s', companionStatus.rotationSpeed);
-      switch (companionStatus.rotationSpeed) {
-        case MiRotationSpeed.AUTO:
-          this.fanService.updateCharacteristic(TargetFanState, TargetFanState.AUTO);
-          this.fanService.updateCharacteristic(RotationSpeed, 3);
-          break;
-        case MiRotationSpeed.FAST:
-          this.fanService.updateCharacteristic(TargetFanState, TargetFanState.MANUAL);
-          this.fanService.updateCharacteristic(RotationSpeed, 3);
-          break;
-        case MiRotationSpeed.NORMAL:
-          this.fanService.updateCharacteristic(TargetFanState, TargetFanState.MANUAL);
-          this.fanService.updateCharacteristic(RotationSpeed, 2);
-          break;
-        case MiRotationSpeed.SLOW:
-          this.fanService.updateCharacteristic(TargetFanState, TargetFanState.MANUAL);
-          this.fanService.updateCharacteristic(RotationSpeed, 1);
-          break;
-      }
-    }
-    if (companionStatus.swingMode !== this.status.swingMode) {
-      this.logger.debug('update swing mode -> %s', companionStatus.swingMode);
-      this.fanService.updateCharacteristic(
-        SwingMode,
-        companionStatus.swingMode === On.ON ? SwingMode.SWING_ENABLED : SwingMode.SWING_DISABLED,
-      );
-    }
-    if (companionStatus.operationMode !== this.status.operationMode) {
-      this.logger.debug('update operation mode -> %s', companionStatus.operationMode);
-      if (companionStatus.power === On.OFF) {
-        this.thermostatService.updateCharacteristic(
-          CurrentHeatingCoolingState,
-          CurrentHeatingCoolingState.OFF,
-        );
-        this.fanService.updateCharacteristic(Active, Active.INACTIVE);
-      } else {
-        switch (companionStatus.operationMode) {
-          case OperationMode.AUTO:
-          case OperationMode.COOLING:
-          case OperationMode.DEHUMIDIFICATION:
-          case OperationMode.SCAVENGER:
-            this.thermostatService.updateCharacteristic(
-              CurrentHeatingCoolingState,
-              CurrentHeatingCoolingState.COOL,
-            );
-            break;
-          case OperationMode.HEATING:
-            this.thermostatService.updateCharacteristic(
-              CurrentHeatingCoolingState,
-              CurrentHeatingCoolingState.HEAT,
-            );
-            break;
-        }
-      }
-    }
+    const shouldUpdate = Object.keys(this.status).some(
+      key =>
+        companionStatus[key as keyof AirConditionerState] !==
+        this.status[key as keyof AirConditionerState],
+    );
     this.status = companionStatus;
+
+    if (!shouldUpdate) {
+      return;
+    }
+    thermostatService.updateCharacteristic(CurrentTemperature, this.targetTemperature);
+    thermostatService.updateCharacteristic(TargetTemperature, this.targetTemperature);
+    thermostatService.updateCharacteristic(TargetHeatingCoolingState, this.targetTemperature);
+    thermostatService.updateCharacteristic(
+      CurrentHeatingCoolingState,
+      this.currentHeatingCoolingState,
+    );
+    fanService.updateCharacteristic(Active, this.active);
+    fanService.updateCharacteristic(TargetFanState, this.targetTemperature);
+    fanService.updateCharacteristic(RotationSpeed, this.rotationSpeed);
+    fanService.updateCharacteristic(SwingMode, this.swingMode);
+    fanService.updateCharacteristic(Active, this.active);
   };
+
+  get targetTemperature() {
+    return this.status.temperature;
+  }
+
+  get active() {
+    return this.status.power === On.ON ? Active.ACTIVE : Active.INACTIVE;
+  }
+
+  get currentHeatingCoolingState() {
+    const { power, operationMode } = this.status;
+    if (power === On.OFF) {
+      return CurrentHeatingCoolingState.OFF;
+    }
+    switch (operationMode) {
+      case OperationMode.SCAVENGER:
+      case OperationMode.DEHUMIDIFICATION:
+      case OperationMode.HEATING:
+        return TargetHeatingCoolingState.HEAT;
+      case OperationMode.AUTO:
+      case OperationMode.COOLING:
+        return TargetHeatingCoolingState.COOL;
+    }
+  }
+
+  get targetHeatingCoolingState() {
+    const { power, operationMode } = this.status;
+    if (power === On.OFF) {
+      return TargetHeatingCoolingState.OFF;
+    }
+    switch (operationMode) {
+      case OperationMode.AUTO:
+      case OperationMode.SCAVENGER:
+      case OperationMode.DEHUMIDIFICATION:
+        return TargetHeatingCoolingState.AUTO;
+      case OperationMode.HEATING:
+        return TargetHeatingCoolingState.HEAT;
+      case OperationMode.COOLING:
+        return TargetHeatingCoolingState.COOL;
+    }
+  }
+
+  get targetFanState() {
+    const { TargetFanState } = this.homebridge.hap.Characteristic;
+    switch (this.status.rotationSpeed) {
+      case MiRotationSpeed.AUTO:
+        return TargetFanState.AUTO;
+        break;
+      case MiRotationSpeed.FAST:
+      case MiRotationSpeed.NORMAL:
+      case MiRotationSpeed.SLOW:
+        return TargetFanState.MANUAL;
+    }
+  }
+
+  get rotationSpeed() {
+    const { power, rotationSpeed } = this.status;
+    if (power === On.OFF) {
+      return 0;
+    }
+    switch (rotationSpeed) {
+      case MiRotationSpeed.AUTO:
+      case MiRotationSpeed.FAST:
+        return 3;
+      case MiRotationSpeed.NORMAL:
+        return 2;
+      case MiRotationSpeed.SLOW:
+        return 1;
+    }
+  }
+
+  get swingMode() {
+    return this.status.swingMode === On.ON ? SwingMode.SWING_ENABLED : SwingMode.SWING_DISABLED;
+  }
+
+  private getTargetHeatingCoolingState = (callback: CharacteristicGetCallback) =>
+    callback(null, this.targetHeatingCoolingState);
 
   private setTargetHeatingCoolingState = async (
     state: CharacteristicValue,
@@ -157,16 +184,24 @@ export class AirConditionerCompanionAccessory implements AccessoryPlugin {
   ) => {
     try {
       this.logger.debug('set target cooler state: %s', state);
+      if (state === this.targetHeatingCoolingState) {
+        this.logger.debug('target cooler state is the same, aborting');
+        callback(null);
+        return;
+      }
       const { TargetHeatingCoolingState } = Characteristic;
       switch (state) {
         case TargetHeatingCoolingState.OFF:
           await this.device.setPower(On.OFF);
+          this.status.power = On.OFF;
           break;
         case TargetHeatingCoolingState.COOL:
           await this.device.setAirConditionerMode(OperationMode.COOLING);
+          this.status.operationMode = OperationMode.COOLING;
           break;
         case TargetHeatingCoolingState.AUTO:
           await this.device.setAirConditionerMode(OperationMode.AUTO);
+          this.status.operationMode = OperationMode.AUTO;
           break;
       }
       callback(null);
@@ -175,6 +210,9 @@ export class AirConditionerCompanionAccessory implements AccessoryPlugin {
     }
   };
 
+  private getTargetTemperature = async (callback: CharacteristicGetCallback) =>
+    callback(null, this.targetTemperature);
+
   private setTargetTemperature = async (
     state: CharacteristicValue,
     callback: CharacteristicSetCallback,
@@ -182,6 +220,7 @@ export class AirConditionerCompanionAccessory implements AccessoryPlugin {
     try {
       this.logger.debug('set target temperature: %s', state);
       await this.device.setTargetTemperature(state as number);
+      this.status.temperature = state as number;
       callback(null);
     } catch (err) {
       callback(err);
@@ -195,12 +234,17 @@ export class AirConditionerCompanionAccessory implements AccessoryPlugin {
     }
     try {
       this.logger.debug('set active: %s', state);
-      await this.device.setPower(state === Characteristic.Active.ACTIVE ? On.ON : On.OFF);
+      const power = state === Characteristic.Active.ACTIVE ? On.ON : On.OFF;
+      await this.device.setPower(power);
+      this.status.power = power;
       callback(null);
     } catch (err) {
       callback(err);
     }
   };
+
+  private getSwingMode = async (callback: CharacteristicGetCallback) =>
+    callback(null, this.swingMode);
 
   private setSwingMode = async (
     state: CharacteristicValue,
@@ -209,12 +253,21 @@ export class AirConditionerCompanionAccessory implements AccessoryPlugin {
     try {
       const { SwingMode } = Characteristic;
       this.logger.debug('set swing mode: %s', state);
-      await this.device.setSwing(state === SwingMode.SWING_ENABLED ? On.ON : On.OFF);
+      if (state === this.swingMode) {
+        this.logger.debug('target swing mode is the same as current one. Aborting');
+        return callback(null);
+      }
+      const swingMode = state === SwingMode.SWING_ENABLED ? On.ON : On.OFF;
+      await this.device.setSwing(swingMode);
+      this.status.swingMode = swingMode;
       callback(null);
     } catch (err) {
       callback(err);
     }
   };
+
+  private getTargetFanState = (callback: CharacteristicGetCallback) =>
+    callback(null, this.targetFanState);
 
   private setTargetFanState = async (
     state: CharacteristicValue,
@@ -223,14 +276,18 @@ export class AirConditionerCompanionAccessory implements AccessoryPlugin {
     try {
       const { TargetFanState } = Characteristic;
       this.logger.debug('set target fan state: %s', state);
-      await this.device.setRotationSpeed(
-        state === TargetFanState.MANUAL ? MiRotationSpeed.NORMAL : MiRotationSpeed.AUTO,
-      );
+      const rotationSpeed =
+        state === TargetFanState.MANUAL ? MiRotationSpeed.NORMAL : MiRotationSpeed.AUTO;
+      await this.device.setRotationSpeed(rotationSpeed);
+      this.status.rotationSpeed = rotationSpeed;
       callback(null);
     } catch (err) {
       callback(err);
     }
   };
+
+  private getRotationSpeed = (callback: CharacteristicGetCallback) =>
+    callback(null, this.rotationSpeed);
 
   private setRotationSpeed = async (
     state: CharacteristicValue,
@@ -238,20 +295,27 @@ export class AirConditionerCompanionAccessory implements AccessoryPlugin {
   ) => {
     try {
       this.logger.debug('set rotation speed: %s', state);
+      let rotationSpeed: MiRotationSpeed;
       switch (state) {
         case 0:
-          await this.device.setRotationSpeed(MiRotationSpeed.AUTO);
-          break;
+          this.device.setPower(On.OFF);
+          this.status.power = On.OFF;
+          callback(null);
+          return;
         case 1:
-          await this.device.setRotationSpeed(MiRotationSpeed.SLOW);
+          rotationSpeed = MiRotationSpeed.SLOW;
           break;
         case 2:
-          await this.device.setRotationSpeed(MiRotationSpeed.NORMAL);
+          rotationSpeed = MiRotationSpeed.NORMAL;
           break;
         case 3:
-          await this.device.setRotationSpeed(MiRotationSpeed.FAST);
+          rotationSpeed = MiRotationSpeed.FAST;
           break;
+        default:
+          throw new Error('Invalid target rotation speed: ' + state);
       }
+      await this.device.setRotationSpeed(rotationSpeed);
+      this.status.rotationSpeed = rotationSpeed;
       callback(null);
     } catch (err) {
       callback(err);
@@ -276,6 +340,7 @@ export class AirConditionerCompanionAccessory implements AccessoryPlugin {
           TargetHeatingCoolingState.AUTO,
         ],
       })
+      .on(CharacteristicEventTypes.GET, this.getTargetHeatingCoolingState)
       .on(CharacteristicEventTypes.SET, this.setTargetHeatingCoolingState);
     this.thermostatService
       .getCharacteristic(Characteristic.CurrentTemperature)
@@ -283,19 +348,23 @@ export class AirConditionerCompanionAccessory implements AccessoryPlugin {
     this.thermostatService
       .getCharacteristic(TargetTemperature)
       .setProps({ maxValue: 30, minValue: 16, minStep: 1 })
+      .on(CharacteristicEventTypes.GET, this.getTargetTemperature)
       .on(CharacteristicEventTypes.SET, this.setTargetTemperature);
 
     this.fanService.getCharacteristic(Active).on(CharacteristicEventTypes.SET, this.setActive);
     this.fanService
       .addCharacteristic(Characteristic.TargetFanState)
+      .on(CharacteristicEventTypes.GET, this.getTargetFanState)
       .on(CharacteristicEventTypes.SET, this.setTargetFanState);
 
     this.fanService
       .addCharacteristic(Characteristic.SwingMode)
+      .on(CharacteristicEventTypes.GET, this.getSwingMode)
       .on(CharacteristicEventTypes.SET, this.setSwingMode);
     this.fanService
       .addCharacteristic(Characteristic.RotationSpeed)
       .setProps({ minValue: 0, maxValue: 3, minStep: 1, format: Formats.INT })
+      .on(CharacteristicEventTypes.GET, this.getRotationSpeed)
       .on(CharacteristicEventTypes.SET, this.setRotationSpeed);
 
     return [this.thermostatService, this.fanService];
